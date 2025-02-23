ace159aab5a818c774b734db6c2d6fb3ed2b6b31
verba4182 <verba4182@mail.ru>
2025-02-23 22:14:14 +0300

    fixed flake8

--- course/tests.py
+++ course/tests.py
@@ -4,7 +4,6 @@ from rest_framework.test import APITestCase
 
 from course.models import Lesson, Course
 from users.models import User
-from django.test import TestCase
 
 
 class LessonTestCase(APITestCase):
